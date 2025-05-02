efe2133eb4f73051450bdd9501123376fca86e85
omkarkunale <kunale2025@gmail.com>
2025-05-02 12:08:51 +0530

    Update demo1.java
    add numbers

--- demo1/demo1.java
+++ demo1/demo1.java
@@ -1,5 +1,9 @@
 class demo1 {
+    public static void add(int a, int b){
+        System.out.println(a+b);
+    }
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        add(3,4);
     }
 }
